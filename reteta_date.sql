3fc5a7b59cb2e37b85298f12311758388a2eaa38
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-10-16 08:45:27 +0300

    Start DMBS project, adjustments to DB project

---
+++ reteta_date.sql
@@ -0,0 +1,5 @@
+INSERT INTO RETETA VALUES(1, 'pizza', 20.99);
+INSERT INTO RETETA VALUES(2, 'pizza rapida', 15.99);
+INSERT INTO RETETA VALUES(3, 'cartofi prajiti', 10.99);
+INSERT INTO RETETA VALUES(4, 'paine proaspata', 5.99);
+INSERT INTO RETETA VALUES(5, 'cartofi prajiti cu sos', 12.99);
---
+++ sterg_tabele.sql
@@ -0,0 +1,5 @@
+INSERT INTO departments VALUES(1, 'pizza', 20.99);
+INSERT INTO departments VALUES(2, 'pizza rapida', 15.99);
+INSERT INTO departments VALUES(3, 'cartofi prajiti', 10.99);
+INSERT INTO departments VALUES(4, 'paine proaspata', 5.99);
+INSERT INTO departments VALUES(5, 'cartofi prajiti cu sos', 12.99);
