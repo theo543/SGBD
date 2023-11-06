c99f2073977ab52cdfbfd0f8bf31f27a28b90772
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-06 12:38:47 +0200

    video db scripts

---
+++ db_create_scripts/del_video.sql
@@ -0,0 +1,5 @@
+drop table TITLE cascade constraints;
+drop table TITLE_COPY cascade constraints;
+drop table MEMBER cascade constraints;
+drop table RENTAL cascade constraints;
+drop table RESERVATION cascade constraints;
