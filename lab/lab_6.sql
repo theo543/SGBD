b4c89a20fb2b97393747792a562e2acc9d617578
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-06 13:43:33 +0200

    pt. b ex 2

--- lab/lab_6.sql
+++ lab/lab_6.sql
@@ -56,11 +56,14 @@ END;
 -- d. Pentru fiecare excursie afișați lista orașelor vizitate.
 -- e. Anulați excursiile cu cele mai puține orașe vizitate.
 
+-- a
+
 CREATE TYPE lista_orase IS TABLE OF VARCHAR2(20);
 CREATE TABLE excursie (
     cod_excursie NUMBER(4) PRIMARY KEY,
     denumire VARCHAR2(20),
-    orase lista_orase
+    orase lista_orase,
+    status VARCHAR(20) DEFAULT 'disponibila'
 )
 NESTED TABLE orase STORE AS NESTED_excursie_lista_orase;
 
@@ -69,6 +72,77 @@ INSERT INTO excursie (cod_excursie, denumire, orase) VALUES (2, 'Excursie 2', li
 INSERT INTO excursie (cod_excursie, denumire, orase) VALUES (3, 'Excursie 3', lista_orase('Craiova', 'Cluj'));
 INSERT INTO excursie (cod_excursie, denumire, orase) VALUES (4, 'Excursie 4', lista_orase('Bucuresti', 'Cluj'));
 INSERT INTO excursie (cod_excursie, denumire, orase) VALUES (5, 'Excursie 5', lista_orase('Craiova', 'Bucuresti', 'Cluj'));
+
+-- b
+
+DECLARE
+    SUBTYPE cod_excursie IS excursie.cod_excursie%TYPE;
+    SUBTYPE oras IS VARCHAR2(20);
+    v_excursie_de_modificat cod_excursie := &p_excursive_de_modificat;
+    v_oras_add_la_sfarsit oras := &p_oras_add_la_sfarsit;
+    v_oras_add_pozitie_2 oras := &p_oras_add_pozitie_2;
+    v_oras_swap_1 oras := &p_oras_swap_1;
+    v_oras_swap_2 oras := &p_oras_swap_2;
+    v_oras_delete oras := &p_oras_delete;
+    v_lista_orase lista_orase;
+    v_lista_orase_crescuta lista_orase := lista_orase();
+    v_oras oras;
+BEGIN
+    SELECT orase
+    INTO v_lista_orase
+    FROM excursie
+    WHERE cod_excursie = v_excursie_de_modificat
+    FOR UPDATE OF orase;
+
+    DBMS_OUTPUT.PUT('Updating: {');
+    FOR i IN 1..v_lista_orase.LAST
+    LOOP
+        DBMS_OUTPUT.PUT(' '||v_lista_orase(i));
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE(' }');
+
+    v_lista_orase.EXTEND;
+    v_lista_orase(v_lista_orase.LAST) := v_oras_add_la_sfarsit;
+
+    v_lista_orase_crescuta.EXTEND(v_lista_orase.COUNT + 1);
+    v_lista_orase_crescuta(1) := v_lista_orase(1);
+    v_lista_orase_crescuta(2) := v_oras_add_pozitie_2;
+    FOR i IN 2..v_lista_orase.LAST
+    LOOP
+        -- DataGrip a insistat sa folosesc DECODE, nu CASE
+        v_lista_orase_crescuta(i + 1) := v_lista_orase(i);
+    END LOOP;
+
+    v_lista_orase.DELETE;
+    FOR i IN 1..v_lista_orase_crescuta.LAST
+    LOOP
+        CONTINUE WHEN v_lista_orase_crescuta(i) = v_oras_delete;
+
+        v_oras := CASE v_lista_orase_crescuta(i)
+            WHEN v_oras_swap_1 THEN v_oras_swap_2
+            WHEN v_oras_swap_2 THEN v_oras_swap_1
+            ELSE v_lista_orase_crescuta(i)
+        END;
+
+        v_lista_orase.EXTEND;
+        v_lista_orase(v_lista_orase.LAST) := v_oras;
+    END LOOP;
+
+    UPDATE excursie
+    SET orase = v_lista_orase
+    WHERE cod_excursie = v_excursie_de_modificat;
+
+    DBMS_OUTPUT.PUT('Updated: {');
+    FOR i IN 1..v_lista_orase.LAST
+    LOOP
+        DBMS_OUTPUT.PUT(' '||v_lista_orase(i));
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE(' }');
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        DBMS_OUTPUT.PUT_LINE('Cod excursie '||v_excursie_de_modificat||' nu exista');
+END;
+/
 
 DROP TABLE excursie;
 DROP TYPE lista_orase;
