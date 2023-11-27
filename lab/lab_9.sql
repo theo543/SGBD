3d9313f82e946db5260a22b58b6f65f8589fe8a4
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-27 14:59:28 +0200

    lab 9 tree query

--- lab/lab_9.sql
+++ lab/lab_9.sql
@@ -247,3 +247,130 @@ SELECT * FROM info_fn_e3;
 DROP FUNCTION fn_e3;
 DROP TABLE info_fn_e3;
 DROP SEQUENCE seq_info_fn_e3;
+
+/*
+    E4. Definiți o procedură stocată care mărește cu 10% salariile tuturor angajaților conduși direct sau
+    indirect de către un manager al cărui cod este dat ca parametru. Tratați cazul în care nu există
+    niciun manager cu codul dat. Inserați în tabelul info_*** informațiile corespunzătoare fiecărui
+    caz determinat de valoarea dată pentru parametru.
+*/
+
+CREATE SEQUENCE seq_tree_proc START WITH 1 INCREMENT BY 1;
+CREATE TABLE info_tree_proc (
+    call_id    NUMBER,
+    iteration  NUMBER,
+    utilizator VARCHAR2(50),
+    data       DATE,
+    root_mgr   NUMBER(6),
+    percent    NUMBER,
+    rezultat   VARCHAR2(100),
+    PRIMARY KEY (call_id, iteration)
+);
+
+CREATE OR REPLACE PROCEDURE tree_proc(v_root_manager employees.manager_id%TYPE, v_percent NUMBER)
+IS
+    v_id BINARY_INTEGER;
+    v_iteration BINARY_INTEGER := 1;
+    v_msg VARCHAR2(100);
+    v_check_exists BINARY_INTEGER;
+
+    CURSOR c_branching_query
+    IS
+        SELECT employee_id, salary
+        FROM employees e1
+        START WITH employee_id = v_root_manager
+        CONNECT BY manager_id = PRIOR employee_id
+        FOR UPDATE;
+
+    PROCEDURE insert_log(v_id NUMBER, v_iteration NUMBER, v_rezultat VARCHAR2)
+    IS
+        BEGIN
+            INSERT INTO info_tree_proc (call_id, iteration, utilizator, data, root_mgr, percent, rezultat)
+            VALUES (v_id, v_iteration, (SELECT USER FROM DUAL), (SELECT SYSDATE FROM DUAL), v_root_manager, v_percent, v_rezultat);
+    END insert_log;
+
+    BEGIN
+
+        SELECT seq_tree_proc.nextval
+        INTO v_id
+        FROM DUAL;
+
+
+        SELECT COUNT(*)
+        INTO v_check_exists
+        FROM employees
+        WHERE employee_id = v_root_manager AND EXISTS(
+            SELECT *
+            FROM employees
+            WHERE manager_id = v_root_manager
+        );
+
+        IF v_check_exists = 0
+        THEN
+            SELECT COUNT(*)
+            INTO v_check_exists
+            FROM employees
+            WHERE employee_id = v_root_manager;
+
+            IF v_check_exists = 0
+            THEN
+                insert_log(v_id, -1, 'Nu există angajat cu ID '||v_root_manager||'.');
+            ELSE
+                insert_log(v_id, -1, 'Angajatul cu ID '||v_root_manager||' nu este manager.');
+            END IF;
+        END IF;
+
+        FOR rezultat IN c_branching_query
+        LOOP
+            CONTINUE WHEN rezultat.employee_id = v_root_manager;
+
+            v_msg := 'Actualizat salariul angajatului '||rezultat.employee_id||' de la '||rezultat.salary||' lei la ';
+            rezultat.salary := rezultat.salary * v_percent;
+            v_msg := v_msg||rezultat.salary||' lei. (procent = '||v_percent||')';
+            insert_log(v_id, v_iteration, v_msg);
+            v_iteration := v_iteration + 1;
+        END LOOP;
+END;
+/
+
+DECLARE
+    v_percent NUMBER := 1.1;
+BEGIN
+    -- testare cu manageri
+    tree_proc(101, v_percent);
+    tree_proc(102, v_percent);
+
+    -- testare cu non-manager
+    tree_proc(206, v_percent);
+
+    -- testare cu id non-existent
+    tree_proc(9999, v_percent);
+END;
+/
+
+SELECT * FROM info_tree_proc;
+
+/*
+1,1,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 108 de la 12000 lei la 13200 lei. (procent = 1,1)"
+1,2,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 109 de la 9000 lei la 9900 lei. (procent = 1,1)"
+1,3,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 110 de la 8200 lei la 9020 lei. (procent = 1,1)"
+1,4,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 111 de la 7700 lei la 8470 lei. (procent = 1,1)"
+1,5,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 112 de la 7800 lei la 8580 lei. (procent = 1,1)"
+1,6,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 113 de la 6900 lei la 7590 lei. (procent = 1,1)"
+1,7,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 200 de la 4400 lei la 4840 lei. (procent = 1,1)"
+1,8,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 203 de la 6500 lei la 7150 lei. (procent = 1,1)"
+1,9,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 204 de la 10000 lei la 11000 lei. (procent = 1,1)"
+1,10,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 205 de la 12000 lei la 13200 lei. (procent = 1,1)"
+1,11,THEO,2023-11-27 14:59:51,101,1.1,"Actualizat salariul angajatului 206 de la 8300 lei la 9130 lei. (procent = 1,1)"
+2,1,THEO,2023-11-27 14:59:51,102,1.1,"Actualizat salariul angajatului 103 de la 9000 lei la 9900 lei. (procent = 1,1)"
+2,2,THEO,2023-11-27 14:59:51,102,1.1,"Actualizat salariul angajatului 104 de la 6000 lei la 6600 lei. (procent = 1,1)"
+2,3,THEO,2023-11-27 14:59:51,102,1.1,"Actualizat salariul angajatului 105 de la 4800 lei la 5280 lei. (procent = 1,1)"
+2,4,THEO,2023-11-27 14:59:51,102,1.1,"Actualizat salariul angajatului 106 de la 4800 lei la 5280 lei. (procent = 1,1)"
+2,5,THEO,2023-11-27 14:59:51,102,1.1,"Actualizat salariul angajatului 107 de la 4200 lei la 4620 lei. (procent = 1,1)"
+3,-1,THEO,2023-11-27 14:59:51,206,1.1,Angajatul cu ID 206 nu este manager.
+4,-1,THEO,2023-11-27 14:59:51,9999,1.1,Nu există angajat cu ID 9999.
+*/
+
+DROP PROCEDURE tree_proc;
+DROP TABLE info_tree_proc;
+DROP SEQUENCE seq_tree_proc;
