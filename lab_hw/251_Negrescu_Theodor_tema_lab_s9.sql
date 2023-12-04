a986f4787da355316147c92ea1a274ce326e0319
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-12-04 04:56:08 +0200

    Add E5+E6 to lab 9 hw

--- lab_hw/251_Negrescu_Theodor_tema_lab_s9.sql
+++ lab_hw/251_Negrescu_Theodor_tema_lab_s9.sql
@@ -76,3 +76,185 @@ END;
 /
 
 DROP PROCEDURE e4_oras_angajat;
+
+
+/*
+E5. Definiți un subprogram care obține pentru fiecare nume de departament ziua din săptămână în
+care au fost angajate cele mai multe persoane, lista cu numele acestora, vechimea și venitul lor
+lunar. Afișați mesaje corespunzătoare următoarelor cazuri:
+- într-un departament nu lucrează niciun angajat;
+- într-o zi din săptămână nu a fost nimeni angajat.
+Observații:
+a.  Numele departamentului și ziua apar o singură dată în rezultat.
+b.  Rezolvați problema în două variante, după cum se ține  cont  sau  nu  de  istoricul  joburilor
+angajaților.
+
+E6. Modificați exercițiul anterior astfel încât lista cu numele angajaților să apară într-un clasament
+creat în funcție de vechimea acestora în departament. Specificați  numărul  poziției din
+clasament  și  apoi  lista  angajaților  care  ocupă acel loc. Dacă doi angajați  au  aceeași  vechime,
+atunci aceștia ocupă aceeași poziție în clasament.
+*/
+
+CREATE TYPE e5_employee IS OBJECT (employee_id NUMBER(6), first_name VARCHAR2(50), last_name VARCHAR2(50), days_hired NUMBER(10), seniority_rank NUMBER(6));
+CREATE TYPE e5_employees IS TABLE OF e5_employee;
+CREATE TYPE e5_empty_days IS TABLE OF NUMBER(6);
+CREATE TYPE e5_return IS OBJECT (empty_days e5_empty_days, max_day NUMBER(1), max_day_employees e5_employees);
+CREATE TYPE emp_hire_date IS OBJECT (employee_id NUMBER(6), hire_date DATE);
+CREATE TYPE emp_hire_dates IS TABLE OF emp_hire_date;
+CREATE TYPE emp_hire_day IS OBJECT (employee_id NUMBER(6), hire_day NUMBER(1));
+CREATE TYPE emp_hire_days IS TABLE OF emp_hire_day;
+
+
+CREATE TABLE weekdays AS
+SELECT 1 day_nr, 'Monday' day_name FROM DUAL
+UNION
+SELECT 2, 'Tuesday' FROM DUAL
+UNION
+SELECT 3, 'Wednesday' FROM DUAL
+UNION
+SELECT 4, 'Thursday' FROM DUAL
+UNION
+SELECT 5, 'Friday' FROM DUAL
+UNION
+SELECT 6, 'Saturday' FROM DUAL
+UNION
+SELECT 7, 'Sunday' FROM DUAL;
+
+CREATE OR REPLACE FUNCTION e5_hire_stats(v_department_name VARCHAR2, use_job_history NUMBER)
+RETURN e5_return
+IS
+    v_retval e5_return := e5_return(e5_empty_days(), 0, e5_employees());
+    v_dep_id NUMBER(4);
+    v_hire_dates emp_hire_dates;
+    v_hire_days emp_hire_days;
+    BEGIN
+        SELECT department_id
+        INTO v_dep_id
+        FROM departments
+        WHERE department_name = v_department_name;
+
+        SELECT emp_hire_date(employee_id, hire_date)
+        BULK COLLECT INTO v_hire_dates
+        FROM employees
+        WHERE department_id = v_dep_id;
+
+        IF use_job_history != 0
+        THEN
+            DECLARE
+                tmp emp_hire_dates := v_hire_dates;
+            BEGIN
+                WITH hire_dates AS (
+                    SELECT employee_id, hire_date
+                    FROM TABLE(tmp)
+                    UNION
+                    SELECT employee_id, jh.end_date AS hire_date
+                    FROM TABLE(tmp)
+                    JOIN job_history jh
+                    USING (employee_id)
+                )
+                SELECT emp_hire_date(t1.employee_id, t1.hire_date)
+                BULK COLLECT INTO v_hire_dates
+                FROM hire_dates t1
+                LEFT JOIN hire_dates t2
+                ON t1.employee_id = t2.employee_id AND t1.hire_date < t2.hire_date
+                WHERE t2.hire_date IS NULL;
+            END;
+        END IF;
+
+        SELECT emp_hire_day(employee_id, TRUNC(hire_date) - TRUNC(hire_date, 'IW') + 1) AS hire_day
+        BULK COLLECT INTO v_hire_days
+        FROM TABLE(v_hire_dates);
+
+        SELECT day_nr
+        BULK COLLECT INTO v_retval.empty_days
+        FROM weekdays
+        LEFT JOIN TABLE(v_hire_days) hire_days
+        ON weekdays.day_nr = hire_days.hire_day
+        WHERE hire_days.hire_day IS NULL;
+
+        IF v_retval.empty_days.LAST = 7 THEN
+            RETURN v_retval;
+        END IF;
+
+        WITH day_counts AS (
+            SELECT hire_day
+            FROM TABLE(v_hire_days)
+            GROUP BY hire_day
+            ORDER BY COUNT(*) DESC
+        )
+        SELECT hire_day
+        INTO v_retval.max_day
+        FROM day_counts
+        WHERE rownum = 1;
+
+        WITH emp_to_rank AS (
+            SELECT employee_id, first_name, last_name, SYSDATE - hire_date AS days_worked
+            FROM employees
+            JOIN TABLE (v_hire_days)
+            USING (employee_id)
+            WHERE hire_day = v_retval.max_day
+            ORDER BY days_worked DESC
+        )
+        SELECT e5_employee(employee_id, first_name, last_name, days_worked, RANK() OVER (ORDER BY days_worked DESC))
+        BULK COLLECT INTO v_retval.max_day_employees
+        FROM emp_to_rank;
+
+        RETURN v_retval;
+END;
+/
+
+DECLARE
+    v_dep_name VARCHAR2(50) := &p_dep_name;
+    v_use_history BOOLEAN := &p_use_history;
+    v_hire_stats e5_return;
+BEGIN
+    dbms_output.PUT_LINE('Stats for department "'||v_dep_name||'". '|| CASE WHEN v_use_history THEN 'U' ELSE 'Not u' END||'sing job history.');
+    v_hire_stats := e5_hire_stats(v_dep_name, CASE WHEN v_use_history THEN 1 ELSE 0 END);
+    IF v_hire_stats.max_day = 0 THEN
+        dbms_output.PUT_LINE('Department has no employees.');
+    ELSE
+        FOR v_row IN (
+            SELECT day_name FROM TABLE(v_hire_stats.empty_days) ed
+            JOIN weekdays
+            ON weekdays.day_nr = ed.column_value
+        )
+        LOOP
+            dbms_output.PUT_LINE('No employees were hired on day '||v_row.day_name||'.');
+        END LOOP;
+        DECLARE
+            v_day_name VARCHAR2(50);
+        BEGIN
+            SELECT day_name
+            INTO v_day_name
+            FROM weekdays
+            WHERE day_nr = v_hire_stats.max_day;
+            dbms_output.PUT_LINE('Weekday '||v_day_name||' has maximum number of hired employees.');
+        END;
+        DECLARE
+            v_current_rank BINARY_INTEGER := 0;
+            v_emp e5_employee;
+        BEGIN
+            FOR i IN 1..v_hire_stats.max_day_employees.last
+            LOOP
+                v_emp := v_hire_stats.max_day_employees(i);
+                IF v_current_rank != v_emp.seniority_rank
+                THEN
+                    v_current_rank := v_emp.seniority_rank;
+                    dbms_output.PUT_LINE('Seniority rank '||v_current_rank||':');
+                END IF;
+                dbms_output.PUT_LINE(v_emp.first_name||' '||v_emp.last_name||' (ID '||v_emp.employee_id||', hired '||v_emp.days_hired||' days ago).');
+            END LOOP;
+        END;
+    END IF;
+END;
+
+DROP FUNCTION e5_hire_stats;
+DROP TYPE emp_hire_days;
+DROP TYPE emp_hire_day;
+DROP TYPE emp_hire_dates;
+DROP TYPE emp_hire_date;
+DROP TYPE e5_return;
+DROP TYPE e5_empty_days;
+DROP TYPE e5_employees;
+DROP TYPE e5_employee;
+DROP TABLE weekdays;
