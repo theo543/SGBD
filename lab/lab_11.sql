75db43492378819289d481e9368865d91fd6a07a
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-12-11 13:21:04 +0200

    lab 11 ex 2

--- lab/lab_11.sql
+++ lab/lab_11.sql
@@ -12,3 +12,34 @@ WHERE department_id = 1;
 
 DROP TRIGGER delete_interzis_user_theo;
 
+UPDATE employees
+SET commission_pct = 0.6
+WHERE employee_id = 100;
+
+CREATE OR REPLACE TRIGGER limita_comision
+    BEFORE UPDATE ON employees
+    FOR EACH ROW
+BEGIN
+    IF  NOT (:old.commission_pct IS NOT NULL AND :old.commission_pct > 0.5)
+        AND (:new.commission_pct IS NOT NULL AND :new.commission_pct > 0.5)
+    THEN
+        raise_application_error(-20011, 'Comisionul nu poate depăși 50%.');
+    END IF;
+END;
+
+UPDATE employees
+SET commission_pct = 0.7
+WHERE employee_id = 100;
+
+UPDATE employees
+SET commission_pct = 2 * commission_pct;
+
+UPDATE employees
+SET commission_pct = NULL
+WHERE employee_id = 100;
+
+UPDATE employees
+SET commission_pct = 0.7
+WHERE employee_id = 100;
+
+DROP TRIGGER limita_comision;
