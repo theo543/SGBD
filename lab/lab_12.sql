cfdeb3189bc6506cf01585fc3f119c66f9ff133d
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-12-18 13:26:16 +0200

    lab 7 e1

--- lab/lab_12.sql
+++ lab/lab_12.sql
@@ -1,4 +1,6 @@
 /*
+Laborator6_PLSQL.pdf
+
 E4. Definiți  un  declanșator cu ajutorul căruia să se implementeze restricția conform căreia într-un
 departament  nu  pot  lucra  mai  mult  de  45  persoane  (se  vor  utiliza  doar  tabelele  emp_***  și
 dept_*** fără a modifica structura acestora).
@@ -124,3 +126,73 @@ SELECT * FROM dpt_count;
 
 DROP MATERIALIZED VIEW dpt_count;
 DROP MATERIALIZED VIEW LOG ON employees;
+
+/*
+Laborator7_PLSQL.pdf
+
+E1. Să se creeze un bloc PL/SQL care afişează radicalul unei variabile introduse de la tastatură. Să
+se trateze cazul în care valoarea variabilei este negativă. Gestiunea  erorii  se  va  realiza  prin 
+definirea unei excepţii de către utilizator, respectiv  prin  captarea  erorii  interne  a  sistemului. 
+Codul şi mesajul erorii vor fi introduse în tabelul error_***(cod, mesaj).
+*/
+
+CREATE OR REPLACE PACKAGE pkg_sqrt
+AS
+    imaginary_numbers_not_supported EXCEPTION;
+    PRAGMA EXCEPTION_INIT (imaginary_numbers_not_supported, -20005);
+    FUNCTION checked_sqrt(x NUMBER) RETURN NUMBER;
+END pkg_sqrt;
+
+CREATE OR REPLACE PACKAGE BODY pkg_sqrt AS
+    FUNCTION checked_sqrt(x NUMBER) RETURN NUMBER
+    AS
+    BEGIN
+        IF x < 0 THEN
+            raise_application_error(-20005, 'sqrt('||x||') would be an imaginary number');
+        END IF;
+        RETURN sqrt(x);
+    END;
+END pkg_sqrt;
+
+CREATE SEQUENCE seq_error_sqrt INCREMENT BY 1 START WITH 1;
+
+CREATE table error_sqrt (
+    id_error_sqrt NUMBER(10) DEFAULT seq_error_sqrt.nextval PRIMARY KEY,
+    code NUMBER(10),
+    msg VARCHAR2(255)
+);
+
+BEGIN
+    FOR num IN -10..10
+    LOOP
+        DECLARE
+            v_result BINARY_INTEGER;
+        BEGIN
+            dbms_output.PUT('Attempting to take square root of number '||num||'...');
+            IF MOD(num, 2) = 0
+            THEN
+                dbms_output.PUT(' using checked_sqrt...');
+                v_result := pkg_sqrt.checked_sqrt(num);
+            ELSE
+                dbms_output.PUT(' using sqrt...');
+                v_result := sqrt(num);
+            END IF;
+            dbms_output.PUT_LINE(' Got result '||v_result);
+        EXCEPTION
+            WHEN OTHERS THEN
+                dbms_output.PUT_LINE(' Exception occured. Logging code '||SQLCODE||' and message '||SQLERRM||' to error_sqrt');
+                DECLARE
+                    v_code error_sqrt.code%TYPE := SQLCODE;
+                    v_errm error_sqrt.msg%TYPE := SQLERRM;
+                BEGIN
+                    INSERT INTO error_sqrt (code, msg) VALUES (v_code, v_errm);
+                END;
+        END;
+    END LOOP;
+END;
+
+SELECT * FROM error_sqrt;
+
+DROP TABLE error_sqrt;
+DROP SEQUENCE seq_error_sqrt;
+DROP PACKAGE pkg_sqrt;
