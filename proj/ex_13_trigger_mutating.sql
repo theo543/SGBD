6e6babfca5bfb8aaf72f8f10303a394412d28eed
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-01-12 22:12:05 +0200

    Fix mutating error test interfering with real trigger

--- proj/ex_13_trigger_mutating.sql
+++ proj/ex_13_trigger_mutating.sql
@@ -1,4 +1,6 @@
-CREATE OR REPLACE TRIGGER limita_angajati
+ALTER TRIGGER limita_angajati DISABLE;
+
+CREATE OR REPLACE TRIGGER limita_angajati_eronat
     AFTER INSERT OR UPDATE ON angajat
     FOR EACH ROW
 BEGIN
@@ -9,4 +11,6 @@ END;
 INSERT INTO angajat (id_restaurant, id_angajator, job_cod, nume, data_angajare, salariu)
 VALUES (1, 1, 'CASIER', 'Ion', SYSDATE, 1000);
 
-DROP TRIGGER limita_angajati;
+DROP TRIGGER limita_angajati_eronat;
+
+ALTER TRIGGER limita_angajati ENABLE;
